a0483e1cc2d35029d554a0da41b1e8e04f16a6ff
antoniojha <antoniojha@gmail.com>
2015-04-03 01:53:03 +0000

    prepare heroku

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,29 +1,17 @@
 class Schedule < ActiveRecord::Base
-
+  attr_accessor :hours, :date
   belongs_to :broker
   belongs_to :user
 
   validates :broker_id, :user_id, presence: true
  # validates_uniqueness_of :time_begin
   
-  def hours=(hour)
-    @hour_begin=hour.split("-").first
-    @hour_end=hour.split("-").last    
-  end
-  def hours
-    if @hour_begin && @hour_end
-    @hour_begin +"-"+@hour_end
-    end
-  end
-  def date=(date)
+  before_validation(:on => :create) do
+    @hour_begin=hours.split("-").first
+    @hour_end=hours.split("-").last   
     @date=date
     self.schedule_date=date
-  end
-  def date
-    @date
-  end
-  before_validation(:on => :create) do
-    self.time_begin=DateTime.strptime(@date+@hour_begin,("%Y-%m-%d%H:%M%p"))
-    self.time_end=DateTime.strptime(@date+@hour_end,("%Y-%m-%d%H:%M%p"))
+    self.time_begin=DateTime.strptime(date+@hour_begin,("%Y-%m-%d%H:%M%p"))
+    self.time_end=DateTime.strptime(date+@hour_end,("%Y-%m-%d%H:%M%p"))
   end
 end
