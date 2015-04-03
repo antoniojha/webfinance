dc714fa2c997cb52c3d3ffa4c07113359f4f488f
antoniojha <antoniojha@gmail.com>
2015-04-03 00:10:23 +0000

    prepare for heroku

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,18 +1,29 @@
 class Schedule < ActiveRecord::Base
-  attr_accessor :date, :hours
+
   belongs_to :broker
   belongs_to :user
   before_validation :create_schedule_times
   validates :broker_id, :user_id, presence: true
   validates_uniqueness_of :time_begin
   
-  
+  def hours=(hour)
+    @hour_begin=hour.split("-").first
+    @hour_end=hour.split("-").last    
+  end
+  def hours
+    if @hour_begin && @hour_end
+    @hour_begin +"-"+@hour_end
+    end
+  end
+  def date=(date)
+    @date=date
+    self.schedule_date=date
+  end
+  def date
+    @date
+  end
   def create_schedule_times
-
-    hour_begin=@hours.split("-").first
-    hour_end=@hours.split("-").last
-    self.time_begin=DateTime.strptime(@date+hour_begin,("%Y-%m-%d%H:%M%p"))
-    self.time_end=DateTime.strptime(@date+hour_end,("%Y-%m-%d%H:%M%p"))
-    self.schedule_date=@date
+    self.time_begin=DateTime.strptime(@date+@hour_begin,("%Y-%m-%d%H:%M%p"))
+    self.time_end=DateTime.strptime(@date+@hour_end,("%Y-%m-%d%H:%M%p"))
   end
 end
