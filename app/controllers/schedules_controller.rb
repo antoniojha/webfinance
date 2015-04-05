e52eff9bd22a12b0484c30c7da4208a8c19e62c5
antoniojha <antoniojha@gmail.com>
2015-04-05 13:48:24 +0000

    prepare heroku

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,5 +1,5 @@
 class SchedulesController < User::AuthenticatedController
-  before_action :set_broker, only: [:new,:create]
+  before_action :set_broker, only: [:new, :create]
   
   def new
     @schedule=Schedule.new
