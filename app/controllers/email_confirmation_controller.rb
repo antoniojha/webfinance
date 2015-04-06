f305c3e0dd6b59204c9c082b50a6b028df61481a
antoniojha <antoniojha@gmail.com>
2015-04-06 23:27:31 +0000

    prepare heroku

--- app/controllers/email_confirmation_controller.rb
+++ app/controllers/email_confirmation_controller.rb
@@ -12,7 +12,7 @@ class EmailConfirmationController < User::AuthenticatedController
       if(!@user)
         redirect_to user_login_url, alert: "Email authentication didn't go through, try again!"
       else
-        if @user.update_attributes(:email_authen, true)
+        if @user.update_attributes(:email_authen=> true)
           user_log_in(@user)
           redirect_to @user, notice: "Email is now authenticated!"
         else
