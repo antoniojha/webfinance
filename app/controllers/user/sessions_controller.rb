c6da137decc539475f4f49a588eeec35c3574d2e
antoniojha <antoniojha@gmail.com>
2015-05-01 11:25:00 +0000

    prepare herok

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -3,6 +3,7 @@ class User::SessionsController < User::AuthenticatedController
   skip_before_action :authorize_user_login, only: [:new, :create,:destroy]
   skip_before_action :remember_location_user, only:[:new]
   def new
+    # serves as blank user object for @user.error.
     @user=User.new
   end
 
@@ -26,7 +27,7 @@ class User::SessionsController < User::AuthenticatedController
         if user && user.has_password?(params[:session][:password])
             user_log_in(user)
             params[:session][:remember]=='1' ? user_remember(user) : user_forget(user)
-            format.html { friendly_redirect user}
+            format.html { friendly_redirect(user,"Successfully Signed In")}
             #  resend email confirmation with a new token if user try to sign in without first authenticating email during sign up
        #     user.send_email_confirmation
         else
