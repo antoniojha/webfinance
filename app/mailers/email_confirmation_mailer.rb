6849ef321ae15922cce09abc65f44599a1f37617
antoniojha <antoniojha@gmail.com>
2015-05-12 00:49:03 +0000

    prepare heroku

--- app/mailers/email_confirmation_mailer.rb
+++ app/mailers/email_confirmation_mailer.rb
@@ -1,5 +1,5 @@
 class EmailConfirmationMailer < ActionMailer::Base
-  default from: "Antonio Jha <antoniojha@gmail.com>"
+  default from: "support@richrly.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
