a6190fc70a8a9793ba7ea2ed8399acf88b00807b
antoniojha <antoniojha@gmail.com>
2015-05-17 03:38:33 +0000

    prepare heroku

--- app/controllers/product_fin_category_rels_controller.rb
+++ app/controllers/product_fin_category_rels_controller.rb
@@ -1,6 +1,6 @@
 class ProductFinCategoryRelsController < ApplicationController
   def new
-    @product=Product.find(params[:product_id].to_i)
+    @product=Product.find(params[:product_id])
     @categories=@product.product_fin_category_rels
     @product_relation=ProductFinCategoryRel.new
   end
