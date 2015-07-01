7c405ece949593ea24728db68ee5083317ab8d1b
antoniojha <antoniojha@gmail.com>
2015-07-01 16:45:28 +0000

    prepare heroku

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,8 +9,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  
+   storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -54,8 +55,8 @@ class ImageUploader < CarrierWave::Uploader::Base
     end
   end
   def get_geometry
-    if (@file)
-      img = ::Magick::Image::read(@file.file).first
+    if (@file.url)
+      img = ::Magick::Image::read(@file.url).first
       @geometry = [ img.columns, img.rows ]
     end
   end
