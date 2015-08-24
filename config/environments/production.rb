62a975b799f242ac6ec997dde6b7429c71180b1b
antoniojha <antoniojha@gmail.com>
2015-08-24 21:18:51 +0000

    prepare heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.assets.raise_runtime_errors = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options={:host=>"richrly.herokuapp.com/"}
   
   config.action_mailer.smtp_settings={
